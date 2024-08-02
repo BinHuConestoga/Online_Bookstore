f76ba7db13a6dc6c8b083a2059bce7dfec73891b
mugiwaranosri <sriramskullzz1999@gmail.com>
2024-08-02 12:56:11 -0400

    Models

---
+++ Online_Bookstore/OBS/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineBookstore.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string UserName { get; set; }
+        public List<Book> Books { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; }
+    }
+}
+s
---
+++ Online_Bookstore/OBS/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineBookstore.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int BookId { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+    }
+}
